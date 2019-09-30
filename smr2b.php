916f13ed9e9ae901e0e5ead2b472763c68dfabd1
MarioPerezRodriguez <marioperezrodriguez02@gmail.com>
2019-09-30 21:26:02 +0200

    php

--- smr2b.php
+++ smr2b.php
@@ -1,3 +1,10 @@
 <a href="http://192.168.0.139/smr2b_David/smr2b.html">David</a>
 Mario
 <a href="http://192.168.0.144/smr2b_fernandoherrero/smr2b.html">Fernando</a>
+
+<?php
+$f=fopen('visitas.txt','a');
+fwrite($f,$_SERVER['REMOTE_ADDR']);
+fwrite($f,"\r\n");
+fclose($f)
+?>
