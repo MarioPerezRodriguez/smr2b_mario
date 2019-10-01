71528ccb7075ee6de1caf778abacd8146da18a6b
MarioPerezRodriguez <marioperezrodriguez02@gmail.com>
2019-10-01 17:58:50 +0200

    php

--- smr2b.php
+++ smr2b.php
@@ -2,10 +2,25 @@
 Mario
 <a href="http://192.168.0.123/smr2b_fernandoherrero/smr2b.php">Fernando</a>
 
+
+<form>
+<input name = "email" type = "email">
+<button> email </button>
+</form>
+
 <?php
 $f=fopen('visitas.txt','a');
 fwrite($f,date('Y-m-d H:i:s '));
 fwrite($f,$_SERVER['REMOTE_ADDR']);
 fwrite($f,"\r\n");
-fclose($f)
+fclose($f);
+?>
+
+<?php
+if(isset($_GET['email'])){
+	$f=fopen('suscriptores.txt','a');
+	fwrite($f,$_GET['email']);
+	fwrite($f,"\r\n");
+	fclose($f);
+}
 ?>
