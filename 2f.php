acef037f82c405d6e86b04992b062f237e28d2c1
MarioPerezRodriguez <marioperezrodriguez02@gmail.com>
2019-10-11 17:20:35 +0200

    php

---
+++ 2f.php
@@ -0,0 +1,24 @@
+<?php
+function meses($n){
+	$meses=[
+	'enero',
+	'febrero',
+	'marzo',
+	'abril',
+	'mayo',
+	'junio',
+	'julio',
+	'agosto',
+	'septiembre',
+	'octubre',
+	'noviembre',
+	'diciembre',
+		];
+	
+		return $meses[$n-1];
+}
+
+for($i=1;$i<13;$i++){
+		if(!strstr(meses($i),'r'))
+			echo meses($i).'<br>';
+}
---
+++ prueba.php
@@ -0,0 +1,10 @@
+<?php
+
+if (strstr ('mayo','r')){
+	
+	echo 'con r';
+}
+
+else{
+	echo 'sin r';
+}
