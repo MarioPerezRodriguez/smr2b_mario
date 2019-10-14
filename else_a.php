c9dc6e23b24130e1653c0dd7868a3ae945cf9556
MarioPerezRodriguez <marioperezrodriguez02@gmail.com>
2019-10-14 21:12:19 +0200

    php

---
+++ else_a.php
@@ -0,0 +1,8 @@
+<?php
+
+$nota = 6;
+$r = 'APTO';
+if($nota<5)
+	$r= 'NO APTO';
+	
+	echo $r;
---
+++ else_b.php
@@ -0,0 +1,15 @@
+<?php
+
+$nota = 8.5;
+
+
+$r= 'PENDIENTE';
+if ($nota>=5)
+	$r= 'APROBADO';
+if($nota>=7)
+	$r='NOTABLE';
+if ($nota>=8.5)
+	$r= 'SOBRESALIENTE';
+
+	
+	echo $r;
