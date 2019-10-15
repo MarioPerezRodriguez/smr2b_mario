f38b77e0db686bc021e90c4b231f2768c8841d2a
MarioPerezRodriguez <marioperezrodriguez02@gmail.com>
2019-10-15 17:36:07 +0200

    php

---
+++ html.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Web de Mario</title>
+</head>
+<body >
+<h1>Documento HTML de Mario<h1>
+
+<h2>Capítulo 1: La web</h2>
+
+<h3>2.2: ¿Cómo empezó todo?</h3>
+
+<font color="red"> <font size="2"><h4>2.3: ¿Cómo finalizo todo?</h4></font></font>
+
+<form>
+
+<input type="text">
+<input type="submit">
+
+</form>
+
+<br>
+<table border>
+  <tr><td>Galicia</td><td>La Coruña</td></tr>
+  <tr><td>Galicia</td><td>Lugo</td></tr>
+  <tr><td>Galicia</td><td>Orense</td></tr>
+  <tr><td>Galicia</td><td>Pontevedra</td></tr>
+  <tr><td>Asturias</td><td>Gijón</td></tr>
+  <tr><td>Asturias</td><td>Gijón</td></tr>
+  <tr><td>Cantabria</td><td>Cantabria</td></tr>
+  <tr><td>Pais Vasco</td><td>Álava</td></tr>
+  <tr><td>Pais Vasco</td><td>Vizcaya</td></tr>
+  <tr><td>Pais Vasco</td><td>Guipúzcoa</td></tr>
+  
+	
+	</table>
+<center>
+<b><u>En un lugar de La Mancha</b></u>, de cuyo nombre no quiero acordarme...
+<br>
+<img src="https://estaticos.muyhistoria.es/media/cache/400x300_thumb/uploads/images/pyr/58763d8b5cafe8e5bd8b45bc/quijoteysancho-c_0.jpg" width="400">
+
+
+<p><?php echo file_get_contents('http://loripsum.net/api');?></p>
+
+</center>
+
+</body>
+</html>
