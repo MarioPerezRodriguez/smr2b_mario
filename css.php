f84e79f2620b752697e51c236375e67e510018b7
MarioPerezRodriguez <marioperezrodriguez02@gmail.com>
2019-10-15 18:25:57 +0200

    php

---
+++ css.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Web de Mario</title>
+</head>
+
+<body style ="background-color:pink;
+	text-align:center;
+">
+
+<div style="margin:120px;padding;120px">
+<table><tr><td>
+<h1>Documento HTML de Mario<h1>
+
+<h2>Capítulo 1: La web</h2>
+
+<h3>2.2: ¿Cómo empezó todo?</h3>
+
+<h4 style="
+	color:red;
+	font-size:.9em;
+">2.3: ¿Cómo finalizo todo?</h4>
+
+<form>
+
+<input type="text">
+<input type="submit">
+
+</form>
+
+<p></p>
+
+<table style="
+	 border: 1px solid black;
+">
+  <tr><td>Galicia</td><td>La Coruña</td></tr>
+  <tr><td>Galicia</td><td>Lugo</td></tr>
+  <tr><td>Galicia</td><td>Orense</td></tr>
+  <tr><td>Galicia</td><td>Pontevedra</td></tr>
+  <tr><td>Asturias</td><td>Gijón</td></tr>
+  <tr><td>Asturias</td><td>Gijón</td></tr>
+  <tr><td>Cantabria</td><td>Cantabria</td></tr>
+  <tr><td>Pais Vasco</td><td>Álava</td></tr>
+  <tr><td>Pais Vasco</td><td>Vizcaya</td></tr>
+  <tr><td>Pais Vasco</td><td>Guipúzcoa</td></tr>
+  
+	
+	</table>
+<div style="
+	text-align:center;
+">
+
+<span style="
+	font-weight:bold;
+	text-decoration: underline;
+">En un lugar de La Mancha</span>, de cuyo nombre no quiero acordarme...
+<p>
+<img src="https://estaticos.muyhistoria.es/media/cache/400x300_thumb/uploads/images/pyr/58763d8b5cafe8e5bd8b45bc/quijoteysancho-c_0.jpg" width="400">
+
+
+<p><?php echo file_get_contents('http://loripsum.net/api');?></p>
+
+</div>
+</td></tr></table>
+</div>
+</body>
+</html>
---
+++ style.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Web de Mario</title>
+<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body >
+<h1>Documento HTML de Mario<h1>
+
+<h2>Capítulo 1: La web</h2>
+
+<h3>2.2: ¿Cómo empezó todo?</h3>
+
+<h4>2.3: ¿Cómo finalizo todo?</h4>
+
+<form>
+
+<input type="text">
+<input type="submit">
+
+</form>
+
+<p></p>
+
+<table>
+  <tr><td>Galicia</td><td>La Coruña</td></tr>
+  <tr><td>Galicia</td><td>Lugo</td></tr>
+  <tr><td>Galicia</td><td>Orense</td></tr>
+  <tr><td>Galicia</td><td>Pontevedra</td></tr>
+  <tr><td>Asturias</td><td>Gijón</td></tr>
+  <tr><td>Asturias</td><td>Gijón</td></tr>
+  <tr><td>Cantabria</td><td>Cantabria</td></tr>
+  <tr><td>Pais Vasco</td><td>Álava</td></tr>
+  <tr><td>Pais Vasco</td><td>Vizcaya</td></tr>
+  <tr><td>Pais Vasco</td><td>Guipúzcoa</td></tr>
+  
+	
+	</table>
+<div class="quijote">
+
+<span class="enfasis">En un lugar de La Mancha</span>, de cuyo nombre no quiero <span class="enfasis">acordarme</span>...
+<p>
+<img src="https://estaticos.muyhistoria.es/media/cache/400x300_thumb/uploads/images/pyr/58763d8b5cafe8e5bd8b45bc/quijoteysancho-c_0.jpg" width="400">
+
+
+<p><?php echo file_get_contents('http://loripsum.net/api');?></p>
+
+</div>
+
+</body>
+</html>
